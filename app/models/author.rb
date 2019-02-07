9f3da2943c47b402cfce9a73e827277e15415475
studioshane <studioshane@gmail.com>
2019-02-07 18:22:44 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, uniqueness: true
+    validates :email, inclusion: {in: %w(@ .)}
+    validates :phone_number, length: {is: 10}
+    validates :phone_number, numericality: true
 end
